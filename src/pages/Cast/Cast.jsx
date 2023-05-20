1193e87f3fa1b805f0a3e410c17c75c5bdd938a8
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-05-20 18:45:04 +0300

    add Cast

---
+++ src/pages/Cast/Cast.jsx
@@ -0,0 +1,58 @@
+import styles from './Cast.module.css';
+import Loader from 'components/Loader/Loader';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCredits, IMAGE_URL } from 'services/api-movies';
+
+
+
+const Cast = () => {
+const [actors, setActors] = useState([]); 
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false); 
+const { movieId } = useParams(); 
+
+useEffect(() => {
+    const getMovieCredits = async () => {
+      setLoading(true);
+      try {
+        const { cast } = await fetchMovieCredits(movieId);
+        setActors(cast);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getMovieCredits();
+  }, [movieId]);
+
+  return (
+    <div>
+        {loading && <Loader />}
+        {error && <p>Something goes wrong</p>}
+        <ul className={styles.list}>
+        {actors.map(actor => (
+            <li className={styles.item} key={actor.id}>
+            <img
+              className={styles.foto}
+              width="200px"
+              src={
+                actor.profile_path
+                  ? IMAGE_URL + actor.profile_path
+                  : `https://bitsofco.de/content/images/2018/12/broken-1.png`
+              }
+              alt={actor.original_name}
+            />
+            <div className={styles.info}>
+              <p>{actor.name}</p>
+              <p>Character: {actor.character}</p>
+            </div>
+            </li>
+        ))}
+        </ul>
+    </div>
+  );
+};
+
+export default Cast;
