676ea1092aa15959bfb1e8d0c3bf3c977c6c081c
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-05-20 19:50:15 +0300

    add Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import styles from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  <nav>
+    <NavLink
+      className={({ isActive }) => (isActive ? styles.active : styles.link)}
+      to="/"
+    >
+      Home
+    </NavLink>
+    <NavLink
+      className={({ isActive }) => (isActive ? styles.active : styles.link)}
+      to="/movies"
+    >
+      Movies
+    </NavLink>
+  </nav>;
+};
+
+export default Navigation;
